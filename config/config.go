f9a906b41ae96e2bccde9a62e11fb17114c571f3
hajarbleh <johnstephanus@ymail.com>
2019-03-21 16:33:14 +0700

    fix default config path

--- config/config.go
+++ config/config.go
@@ -5,9 +5,10 @@ import (
 	"github.com/pkg/errors"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+
+  homedir "github.com/mitchellh/go-homedir"
 )
 
-const defaultConfigPath = "/home/john/.grafcli/config"
 
 type Config struct {
 	Url    string `yaml:"url"`
@@ -15,14 +16,22 @@ type Config struct {
 }
 
 func Read() (*Config, error) {
+  homeDir, err := homedir.Dir()
+  if err != nil {
+    return nil, errors.Wrap(err, fmt.Sprintf("Cannot get home directory"))
+  }
+
+  defaultConfigPath := homeDir + "/.grafcli/config"
 	dat, err := ioutil.ReadFile(defaultConfigPath)
 	if err != nil {
 		return nil, errors.Wrap(err, fmt.Sprintf("Fatal error config file: %s \n", err))
 	}
+
 	c := new(Config)
 	err = yaml.Unmarshal(dat, &c)
 	if err != nil {
 		return nil, errors.Wrap(err, fmt.Sprintf("Unmarshall: %s \n", err))
 	}
+
 	return c, nil
 }
